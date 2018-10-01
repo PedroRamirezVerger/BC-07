f5c49f106e947ece2bbb5589a95aa64dfe270a42
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2018-10-01 09:29:35 +0200

    Estructura tarea1

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
 public class Grafo {
 
+	public boolean perteneceNodo(int id) {
+		
+		
+		return false;
+	}
+	
+	 public double[] posicionNodo(int id) {
+		
+		 
+		 return null; 
+	 }
+	 
+	 public ArrayList<Arista> adyacentesNodo(int id){
+		
+		 
+		 return null; 
+	 }
 }
