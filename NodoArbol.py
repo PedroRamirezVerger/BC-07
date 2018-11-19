02cffa25bf5dfe2ca37a61d74838f3dbf9df9619
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2018-11-19 11:27:59 +0100

    Update NodoArbol.py

--- NodoArbol.py
+++ NodoArbol.py
@@ -2,13 +2,14 @@ from random import randint
 from Estado import Estado
 
 class NodoArbol:
-	def __init__(self, estado, padre, costo, f):
-		self.padre
-		self.estado
-		self.costo
-		self.accion
-		if padre==NONE:
+	def __init__(self, estado, padre, costo, accion, f):
+		
+		self.nPadre=padre
+		self.nEstado=estado
+		self.nCosto=costo
+		self.nAccion=accion
+		if padre==None:
 			self.profundidad=0
 		else :
-			self.profundidad=padre.profundidad+1
-		self.f=randint(0,100)
+			self.profundidad=nPadre.profundidad+1
+		self.nF=f
