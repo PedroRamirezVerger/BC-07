625fe2a66c5ddcde38752c33939935695d9c88fc
PedroRamirezVerger <32263959+PedroRamirezVerger@users.noreply.github.com>
2018-11-22 21:23:09 +0100

    erores de indentación

--- Main.py
+++ Main.py
@@ -12,88 +12,77 @@ import json
 fAnchura=0
 
 def calcularF(estrategia, profundidad, costo):
-    if estrategia=='c':
-        f=costo
-    if estrategia=='a':
-        f=fAnchura+1
-    if estrategia=='p':
-    	f=0
-    
-    return f
+	if estrategia=='c':
+		f=costo
+	if estrategia=='a':
+		f=fAnchura+1
+	if estrategia=='p':
+		f=0
+	return f
 
 
 def crearSolucion(n_actual, estrategia):
-    file=open("solucion.txt", "w")
-    costo_Total=n_actual.nCosto
-    n_padre=n_actual.nPadre
-    print("aaaaaaaaa")
-    print(n_actual.nPadre)
-    print(n_padre)
+	file=open("solucion.txt", "w")
+	costo_Total=n_actual.nCosto
+	n_padre=n_actual.nPadre
+	listaSolucion=[]
+	listaSolucion.append(n_actual)
+	while n_padre.profundidad != 0:
+		listaSolucion.append(n_padre)
+		costo_Total+=n_padre.nCosto
+		n_padre=n_padre.nPadre
 
-    listaSolucion=[]
-    listaSolucion.append(n_actual)
-    while n_padre.profundidad != 0:
-        listaSolucion.append(n_padre)
-        costo_Total+=n_padre.nCosto
-        n_padre=n_padre.nPadre
-
-    costo_Total+=n_padre.costo
-    listaSolucion.append(n_padre)
-    listaSolucion.reverse()
-    file.write("La solucion es:\nEstrategia:"+estrategia+"\nTotal nodos generados: "+len(listaSolucion)+"\nProfundidad: "+n_actual.profundidad+"\nCosto: "+costo_Total+"\n\n")
-    for n in listaSolucion:
-    	file.write(n.nAccion+" "+n.nCosto+" "+n.profundidad+" "+n.nCosto)
-        file.write("Estoy en "+n.nEstado.nActual+" y tengo que visitar "+n.nEstado.nPendientes) #nodo + lista de pendientes
-    return 0
+	costo_Total+=n_padre.costo
+	listaSolucion.append(n_padre)
+	listaSolucion.reverse()
+	file.write("La solucion es:\nEstrategia:"+estrategia+"\nTotal nodos generados: "+len(listaSolucion)+"\nProfundidad: "+n_actual.profundidad+"\nCosto: "+costo_Total+"\n\n")
+	for n in listaSolucion:
+		file.write(n.nAccion+" "+n.nCosto+" "+n.profundidad+" "+n.nCosto)
+		file.write("Estoy en "+n.nEstado.nActual+" y tengo que visitar "+n.nEstado.nPendientes) #nodo + lista de pendientes
+	return 0
 
 def crearListaNodos(listaSucesores, n_actual, prof_Max, estrategia):
-    listaNodos=[]
-    for sucesor in listaSucesores:
-        nodoArbol=NodoArbol(sucesor['estado'], n_actual, sucesor['costo'], sucesor['accion'], calcularF(estrategia, n_actual.profundidad, sucesor['costo']))
-        if nodoArbol.profundidad<=prof_Max:
-            listaNodos.append(nodoArbol)
-
-
-    return listaNodos
-
-
-
-### LOS PADRES ESTÁN VACÍIIIIIIIIIIIIIOSSSSSSSSS
-
+	listaNodos=[]
+	for sucesor in listaSucesores:
+		nodoArbol=NodoArbol(sucesor['estado'], n_actual, sucesor['costo'], sucesor['accion'], calcularF(estrategia, n_actual.profundidad, sucesor['costo']))
+		if nodoArbol.profundidad<=prof_Max:
+			listaNodos.append(nodoArbol)
+	return listaNodos
+### LOS PADRES ESTaN VACIIIIIIIIIIIIIOSSSSSSSSS
 def busqueda_Acotada(prob,estrategia,prof_Max):
-    frontera=Frontera()
-    n_inicial=NodoArbol(prob.estadoInicial,None,0,None,0)# cambiar clase nodo NodoArbol
-    frontera.insertar(n_inicial)
-    solucion=False
-
-    while (solucion==False) and (not frontera.esVacia()):
-        n_actual=frontera.elimina()
-      
-        if (prob.esObjetivo(n_actual.nEstado)):
-            solucion=True
-        else:
-            listaSucesores=prob.espacioEstados.sucesores(n_actual.estado)
-            listaNodos=crearListaNodos(listaSucesores,n_actual,prof_Max,estrategia) 
-            for n in listaNodos:
-				frontera.insertar(n) # anadir un insertar lista a la frontera o insertarlos uno a uno????
-    if (solucion==True):
-    	print(n_actual.nPadre)
-        return crearSolucion(n_actual, estrategia) 
-    else:
-        return None
+	frontera=Frontera()
+	n_inicial=NodoArbol(prob.estadoInicial,None,0,None,0)# cambiar clase nodo NodoArbol
+	frontera.insertar(n_inicial)
+	solucion=False
+	while solucion==False and frontera.esVacia()!= False:
+		print("ieeeeeeeeee")
+		n_actual=frontera.elimina()
+		print(n_actual)
+		solucion=True
+		if prob.esObjetivo(n_actual.nEstado):
+			solucion=True
+		else:
+			listaSucesores=prob.espacioEstados.sucesores(n_actual.estado)
+			listaNodos=crearListaNodos(listaSucesores,n_actual,prof_Max,estrategia) 
+			for n in listaNodos:
+				frontera.insertar(n)
+				print(n)
+	if solucion==True:
+		print(n_actual.nEstado.nActual)
+		return crearSolucion(n_actual, estrategia)
+	else:
+		return None
 
 def busqueda(prob,estrategia,prof_Max,inc_Prof):
-    profActual=inc_Prof
-    solucion=False
-    while(solucion==False and (profActual<=prof_Max)):
-        solucion=busqueda_Acotada(prob,estrategia,profActual)
-        profActual=profActual+inc_Prof
-    return solucion
-
-
+	profActual=inc_Prof
+	solucion=False
+	while solucion==False and profActual<=prof_Max:
+		solucion=busqueda_Acotada(prob,estrategia,profActual)
+		profActual=profActual+inc_Prof
+	return solucion
 
 with open('problema.json') as f:
-    data=json.load(f)
+	data=json.load(f)
 archivo=data['graphmlfile']
 espacioInicial=data['IntSt']
 problema=Problema(archivo, espacioInicial['node'], espacioInicial['listNodes'])
